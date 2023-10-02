73dd214b288675d87224e24f28e734ebe8ba01be
Anton Borisov <Antonborisov571@gmail.com>
2023-10-03 00:36:10 +0300

    [HW2.1.4] Добавил точку входу и реализацию main

--- Homework4/Hw4/Program.fs
+++ Homework4/Hw4/Program.fs
@@ -1,1 +1,10 @@
-﻿raise (System.NotImplementedException())
+﻿open Hw4
+
+[<EntryPoint>]
+let main (args : string[]) =
+    try
+        let calc = Parser.parseCalcArguments args
+        printf "%f" (Calculator.calculate calc.arg1 calc.operation calc.arg2)
+    with
+        | ex -> printf "%s" (ex.Message)
+    0
