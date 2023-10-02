499237882237d87569ffdb46e627d6b0827c5fea
Anton Borisov <Antonborisov571@gmail.com>
2023-10-03 00:35:18 +0300

    [HW2.1.4] Добавил реализацию парсера

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -11,10 +11,26 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    args.Length = 3
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> ArgumentException("Entered incorrectly operation") |> raise
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    if (isArgLengthSupported args <> true) then ArgumentException("Incorrect number of arguments entered") |> raise
+
+    match Double.TryParse args[0] with
+    | true, arg1 -> None |> ignore
+    | _ -> ArgumentException("Incorrect first value entered") |> raise
+
+    match Double.TryParse args[2] with
+    | true, arg2 -> None |> ignore
+    | _ -> ArgumentException("Incorrect second value entered") |> raise
+
+    let calc = {arg1 = Double.Parse args[0]; arg2 = Double.Parse args[2]; operation = parseOperation args[1]}
+    calc
