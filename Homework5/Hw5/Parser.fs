b1926de545e8e72d437424e45aa7de92338aff99
antonborisov571 <antonborisov571@gmail.com>
2023-10-12 22:24:59 +0300

    [HW2.1.5] В Parser.fs изменил некоторые функции

--- Homework5/Hw5/Parser.fs
+++ Homework5/Hw5/Parser.fs
@@ -4,18 +4,18 @@ open System
 open Hw5.Calculator
 open Hw5.MaybeBuilder
 
-let isArgLengthSupported (args:string[]): Result<(string * string * string), Message> =
+let isArgLengthSupported (args:string[]): Result<string[], Message> =
     match args.Length = 3 with
-    | true -> Ok (args[0], args[1], args[2])
+    | true -> Ok args
     | false -> Error Message.WrongArgLength
     
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isOperationSupported (arg1, operation, arg2): Result<('a * CalculatorOperation * 'b), Message> =
     match operation with 
-    | "+" -> Ok (arg1, CalculatorOperation.Plus, arg2)
-    | "-" -> Ok (arg1, CalculatorOperation.Minus, arg2)
-    | "*" -> Ok (arg1, CalculatorOperation.Multiply, arg2)
-    | "/" -> Ok (arg1, CalculatorOperation.Divide, arg2)
+    | Calculator.plus -> Ok (arg1, CalculatorOperation.Plus, arg2)
+    | Calculator.minus -> Ok (arg1, CalculatorOperation.Minus, arg2)
+    | Calculator.multiply -> Ok (arg1, CalculatorOperation.Multiply, arg2)
+    | Calculator.divide -> Ok (arg1, CalculatorOperation.Divide, arg2)
     | _ -> Error Message.WrongArgFormatOperation
 
 let parseDouble (arg: string): Result<float, Message> =
@@ -25,21 +25,21 @@ let parseDouble (arg: string): Result<float, Message> =
 
 let parseArgs (args: string[]): Result<(float * CalculatorOperation * float), Message> =
     maybe {
-        let! args = isArgLengthSupported args
-        let! arg1, operation, arg2 = isOperationSupported args
-        let! arg1 = parseDouble arg1
-        let! arg2 = parseDouble arg2
-        return (arg1, operation, arg2)
+        let! arg1 = parseDouble args[0]
+        let! arg2 = parseDouble args[2]
+        let! args = isOperationSupported (arg1, args[1], arg2)
+        return args
     } 
 
 [<System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage>]
 let inline isDividingByZero (arg1, operation, arg2): Result<(float * CalculatorOperation * float), Message> =
-    match operation, arg2.ToString() with
-    | CalculatorOperation.Divide, "0" -> Error Message.DivideByZero
+    match operation, arg2 with
+    | CalculatorOperation.Divide, 0.0 -> Error Message.DivideByZero
     | _ -> Ok (arg1, operation, arg2)
     
 let parseCalcArguments (args: string[]): Result<(float * CalculatorOperation * float), Message> =
     maybe {
+        let! args = isArgLengthSupported args
         let! args = parseArgs args
         let! args = isDividingByZero args
         return  args
