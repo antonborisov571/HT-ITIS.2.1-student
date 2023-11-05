354cb3e2fe2822033c14c10348698f2778f0d0f4
antonborisov571 <antonborisov571@gmail.com>
2023-11-05 18:00:21 +0300

    [HW2.1.8] Добавил реализацию калькулятора

---
+++ Homework8/Hw8/Calculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Hw8.Calculator;
+
+public class Calculator : ICalculator
+{
+    public double Divide(double firstValue, double secondValue)
+    {
+        if (secondValue == 0)
+        {
+            throw new InvalidOperationException(Messages.DivisionByZeroMessage);
+        }
+        return firstValue / secondValue;
+    }
+
+    public double Minus(double val1, double val2) =>
+        val1 - val2;
+
+    public double Multiply(double val1, double val2) =>
+        val1 * val2;
+
+    public double Plus(double val1, double val2) =>
+        val1 + val2;
+}
