3a00a27a7b60b45d89d6ea63b59a9cc609f827d6
antonborisov571 <antonborisov571@gmail.com>
2023-11-22 11:05:04 +0300

    [HW2.1.10] Исправления для codecov

--- Homework10/Hw10/Migrations/ApplicationContextModelSnapshot.cs
+++ Homework10/Hw10/Migrations/ApplicationContextModelSnapshot.cs
@@ -4,11 +4,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace Homework10.Migrations
 {
+    [ExcludeFromCodeCoverage]
     [DbContext(typeof(ApplicationContext))]
     partial class ApplicationContextModelSnapshot : ModelSnapshot
     {
